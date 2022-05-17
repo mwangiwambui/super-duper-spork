a7cabf68bcef36e0860cf348a3529fbfe83b9638
mwangiwambui <wambui54mwangi@gmail.com>
2022-05-17 19:20:05 +0300

    Displayed all entries

--- src/components/pages/ListEntries.js
+++ src/components/pages/ListEntries.js
@@ -6,15 +6,36 @@ const ListEntries = () => {
   console.log(entryJson)
   const [entries, setEntry] = useState([]);
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    setEntry(entryJson)
-  }, [entryJson])
+  //   setEntry(entryJson)
+  // }, [entryJson])
   
   function displayEntries() {
-    return entries.items && entries.items.map(entry => {
-      return entry.length > 0 && <Entry key={entry.date} entryDate={entry} />
-    })
+    return(
+      entryJson.map((item, index) => {
+        return(
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="w-4 p-4">
+              <div className="flex items-center">
+                <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                <label for="checkbox-table-search-1" className="sr-only">checkbox</label>
+              </div>
+            </td>
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+              {item.title}
+            </th>
+            <td className="px-6 py-4">
+              {item.date}
+            </td>
+            <td className="px-6 py-4 text-right">
+              <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+          </tr>
+        )
+      })
+      
+    )
   }
 
 

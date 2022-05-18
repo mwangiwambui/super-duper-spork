7fabedf6f1e518bad2f93650b032ea4e632fd8d2
mwangiwambui <wambui54mwangi@gmail.com>
2022-05-18 23:37:36 +0300

    Made my table clickable

--- src/components/pages/ListEntries.js
+++ src/components/pages/ListEntries.js
@@ -10,13 +10,14 @@ const ListEntries = () => {
   }
 
   const deleteItem = (id) => {
-    setEntry(entryItems.filter(entryItem => {
-      return entryItem.id != id
-    }));
+    setEntry([...entryItems.filter(entryItem => {
+      return entryItem.id !== id
+    })]);
   }
 
   useEffect(() => {
     displayEntries();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [entryItems])
 
   function displayEntries() {
@@ -28,13 +29,13 @@ const ListEntries = () => {
             <td className="w-4 p-4">
               <div className="flex items-center">
                 <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                <label for="checkbox-table-search-1" className="sr-only">checkbox</label>
+                <label className="sr-only">checkbox</label>
               </div>
             </td>
-            <th onClick={() => openEntries(item.id)} scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+            <th onClick={() => openEntries(item.id)} style={{cursor : 'pointer'}}  scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
               {item.title}
             </th>
-            <td onClick={() => openEntries(item.id)} className="px-6 py-4">
+            <td onClick={() => openEntries(item.id)} style={{cursor : 'pointer'}}  className="px-6 py-4">
               {item.date}
             </td>
             <td className="px-6 py-4 text-right">
@@ -60,7 +61,7 @@ const ListEntries = () => {
         <label for="table-search" className="sr-only">Search</label>
         <div className="relative mt-1">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
           </div>
           <input type="text" id="table-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
         </div>
